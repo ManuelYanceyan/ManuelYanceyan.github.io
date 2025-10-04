a8ea280a4d20e86ffa1cf9bf9270594c483f0db8
MANUEL YANCEYAN QUINCHO AYALA <140780351+ManuelYanceyan@users.noreply.github.com>
2025-10-04 15:02:15 -0500

    new comit

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,7 +40,7 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
-    title: 'El blog de Manuel Yanceyan Quincho Ayala',
+    title: 'El blog de Manuel Yanceyan Quincho',
     subtitle: 'Este es mi portafoilio para reporte de aprendisaje de clases de DESARROLLO DE APLICACIONES WEB',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
