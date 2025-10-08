aeef65f3a02e59da677788b4d9b74272747ff099
Manuel Quincho Ayala <manueldendi@gmail.com>
2025-10-08 18:56:14 -0500

    xdfc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // Configuración del sitio
-    export default defineConfig({
-    site: 'https://ManuelYanceyan.github.io',
-    integrations: [
-        tailwind({
-        // Puedes activar el modo 'applyBaseStyles' si usas tipografía personalizada
-        applyBaseStyles: true,
-        }),
-        mdx(),
-        sitemap(),
-    ],
+export default defineConfig({
+site: 'https://ManuelYanceyan.github.io',
+integrations: [
+    tailwind({
+      // Puedes activar el modo 'applyBaseStyles' si usas tipografía personalizada
+    applyBaseStyles: true,
+    }),
+    mdx(),
+    // Eliminar la integración de sitemap si no la necesitas
+    // sitemap(),
+],
 });
